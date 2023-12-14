bcb114bdc770075b4c31bb3d509ddd6c0d4f0119
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-14 23:09:05 +0100

    setting up my dev folders and files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React, { lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import('./views/home'));
+
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/views/home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='bg-black'>Home</div>
+  )
+}
+
+export default Home;
