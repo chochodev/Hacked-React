ef5a521edc1a53a1814f200fefeb54d8d83ff0b8
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-14 23:13:04 +0100

    setting up my dev folders and files

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='bg-black'>Home</div>
+    <div className='bg-black text-yellow-500'>Home</div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+@import url('https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter&family=Poppins&family=Russo+One&family=Source+Code+Pro&display=swap');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
