087ae6ecb133b741ee4e627f5d4f7bf33d33e772
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-16 22:49:16 +0100

    updating the setting up

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <p className="font-inter">Home is not a place but the people you feel most at ease</p>
       <p className="font-fira">Home is not a place but the people you feel most at ease</p>
       <p className="font-russo">Home is not a place but the people you feel most at ease</p>
-      <p className="font-poppins">Home is not a place but the people you feel most at ease</p>
+      <p className="font-source">Home is not a place but the people you feel most at ease</p>
     </div>
   )
 }
