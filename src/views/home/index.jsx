18959403bf804dfb6ee3a0352a295d6dd98feb58
Emmanuel Michael <michochoprogrammer@gmail.com>
2023-12-17 22:19:44 +0100

    updating with the setting up of the project

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from 'components/navbar';
 
 const Home = () => {
   return (
     <div className='bg-black text-yellow-500 font-poppins'>
+      <Navbar />
       <p className="font-poppins">Home is not a place but the people you feel most at ease</p>
       <p className="font-inter">Home is not a place but the people you feel most at ease</p>
       <p className="font-fira">Home is not a place but the people you feel most at ease</p>
